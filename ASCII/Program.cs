a78c1e4f88698a0cf2b23eb836c29e8c2068421c
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2020-11-23 11:11:11 +0100

    modifiche

--- ASCII/Program.cs
+++ ASCII/Program.cs
@@ -6,11 +6,14 @@ namespace ASCII
     {
         static void Main(string[] args)
         {
+            //Console.WriteLine("inserisci una frase");
+            //string unicodestring = Console.ReadLine();
+
             string unicodestring =
                 "this string has 2 characters outside the" +
                 "ASCII range: \n" +
-                "Pi (\u03A0) , and sigma (\u03A0).";
-            Console.WriteLine("stringa originale/oriinal string");
+                "radiazione (\u2622) , and sigma (\u03A0).";
+            Console.WriteLine("stringa originale/original string");
             Console.OutputEncoding = System.Text.Encoding.Unicode;
             Console.WriteLine(unicodestring);
             Console.WriteLine();
